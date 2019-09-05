38fb0b59e5cdd2828cf9bb693a68755dc8c790ba
Samina Trachier <st3225@nyu.edu>
2019-09-05 16:17:59 -0400

    Added a timer

---
+++ Assets/scripts/ShipMove.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMove : MonoBehaviour
+{
+    //put this script on the players character
+
+    private Rigidbody myRigidbody;
+    private Vector3 inputVector; //gets input from update and sends it to physics
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>(); //assign RB ref 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //horizontal input (A/D, Left/Right)
+       // float horizontalInput = Input.GetAxis("Horizontal");
+        //vertical input (W/S, Up/Down)
+        float verticalInput = Input.GetAxis("Vertical");
+        
+        //use horizontal input for turning only
+       // transform.Rotate(0f,0f, horizontalInput * Time.deltaTime * 90f);
+        
+        //put the input data in the "inputVector"
+        inputVector = new Vector3(0f, verticalInput, 0f);
+        
+        //is the player moving faster than 5? normalize
+        if (inputVector.magnitude > 1f)
+        {
+            inputVector = Vector3.Normalize(inputVector);
+        }
+        
+        
+    }
+    
+    //fixed update for physics
+    void FixedUpdate()
+    {
+        myRigidbody.AddForce(inputVector * 10f);
+        Debug.Log("input vector" + inputVector);
+    }
+
+
+}
---
+++ Assets/scripts/timer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour
+{
+    private Image line;
+
+    public float maxTime = 100f;
+
+    private float timeLeft;
+
+    public GameObject timesUpText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        timesUpText.SetActive(false);
+        line = GetComponent<Image>();
+        timeLeft = maxTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+            line.fillAmount = timeLeft / maxTime;
+            
+        }
+        else
+        {
+            timesUpText.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+}
